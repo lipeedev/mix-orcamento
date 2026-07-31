8af7914d4d62eddcd6b08b5a6597af010a7f91a0
lipeedev <anamariafakezaum@gmail.com>
2026-07-31 16:52:55 -0300

    refactor: use logo instead header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 export function Header() {
   return (
-    <header className="flex justify-center w-full max-w-4xl mb-8 border-b border-zinc-800 pb-4">
-      <h1 className="text-xl font-black tracking-tighter uppercase text-blue-800">
-        Vidro<span className="text-orange-400">Mix</span> <span className='text-zinc-200'>Orçamento</span>
+    <header className="items-center flex flex-col w-full max-w-4xl mb-8 border-b border-zinc-800 pb-4">
+
+      <img className="w-66 h-18" src="/vidromix.png" />
+      <h1 className="text-xl font-black tracking-wide uppercase text-white [text-shadow:-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,1px_1px_0_#000]">
+        Orçamento
       </h1>
     </header>
   )
